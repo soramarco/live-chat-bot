a37483d2849e9be5951932e4d8ddce172b9fa5a9
soramarco <165821593+soramarco@users.noreply.github.com>
2026-09-03 14:57:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ class PersonalControlView(discord.ui.View):
             self.toggle_btn.style = discord.ButtonStyle.success
             self.toggle_btn.emoji = "🟢"
 
-    @discord.ui.button(label="Chargement...", style=discord.ButtonStyle.secondary, custom_id="toggle_personal_chat_persistent_v10")
+    @discord.ui.button(label="Chargement...", style=discord.ButtonStyle.secondary, custom_id="toggle_personal_chat_persistent_v12")
     async def toggle_btn(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer()
         username = interaction.user.display_name
@@ -56,7 +56,7 @@ class MainPanelView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Gérer mon Live Chat", emoji="⚙️", style=discord.ButtonStyle.blurple, custom_id="main_manage_btn_persistent_v10")
+    @discord.ui.button(label="Gérer mon Live Chat", emoji="⚙️", style=discord.ButtonStyle.blurple, custom_id="main_manage_btn_persistent_v12")
     async def manage_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.defer(ephemeral=True)
         username = interaction.user.display_name
@@ -143,7 +143,7 @@ async def send_control_message(item, is_active):
                 self.item_ref = item_ref
                 self.stop_button.disabled = not active
 
-            @discord.ui.button(label="Stop", emoji="⏹️", style=discord.ButtonStyle.danger, custom_id="stop_btn_dynamic_v4")
+            @discord.ui.button(label="Stop", emoji="⏹️", style=discord.ButtonStyle.danger, custom_id="stop_btn_dynamic_v6")
             async def stop_button(self, interaction: discord.Interaction, button: discord.ui.Button):
                 await interaction.response.defer()
                 global current_active_item, media_queue
@@ -175,7 +175,7 @@ async def activate_next_item_message(item):
                 super().__init__(timeout=None)
                 self.item_ref = item_ref
 
-            @discord.ui.button(label="Stop", emoji="⏹️", style=discord.ButtonStyle.danger, custom_id="stop_btn_dynamic_active_v4")
+            @discord.ui.button(label="Stop", emoji="⏹️", style=discord.ButtonStyle.danger, custom_id="stop_btn_dynamic_active_v6")
             async def stop_button(self, interaction: discord.Interaction, button: discord.ui.Button):
                 await interaction.response.defer()
                 global current_active_item, media_queue
@@ -237,7 +237,6 @@ def pop_meme():
                 asyncio.run_coroutine_threadsafe(activate_next_item_message(current_active_item), bot.loop)
             else:
                 current_active_item = None
-                
     return jsonify({"status": "success"})
 
 def run_flask():
